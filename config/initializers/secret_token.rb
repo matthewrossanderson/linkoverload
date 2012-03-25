6bd2e52f57ef15a12337c20333b1a3890d14e7ca
Matt Anderson <matthewrossanderson@gmail.com>
2012-03-25 18:26:22 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkoverload::Application.config.secret_token = 'ed71a75e032be5a682ff66f562e718a80c21809c6685ec5dbce9b09382cb8d6005af1537de8630c20af6be2bfa84a705d62850f520b973b0a6235dd901355970'
